e54b321d988d1759b314a24eb3c7663b8fab3322
timothyhart <timothy.hart@my.jcu.edu.au>
2017-03-03 11:31:25 +1000

    Layout testing

--- learning-module/js/game.js
+++ learning-module/js/game.js
@@ -1,7 +1,7 @@
 function GameView() {
     var view = this;
     view.canvas = new fabric.Canvas('bird-box-canvas');
-    view.canvas.setDimensions({width: 500, height: 200});
+    view.canvas.setDimensions({width: 100%, height: 200});
     view.container = $(".game-view");
     view.answerContainer = view.container.find(".answer-card")
     view.cardContainer = view.container.find(".cards-container");
