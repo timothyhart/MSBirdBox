fa2e3cbe32bd5dee99e55b83ead6a37f88e85fee
Tim <timothy.hart@my.jcu.edu.au>
2017-05-26 12:08:57 +1000

    This is stupid

--- learning-module/js/postScore.php
+++ learning-module/js/postScore.php
@@ -1,6 +1,6 @@
 <?php
 require_once ("../../prototype/Admin-panel/classes/DB.php");
-require_once ("../../prototype/Admin-panel/classes/DB.php");
+require_once ("../../prototype/Admin-panel/classes/Config.php");
 $db = DB::getInstance();
 $userID = $_POST['userID'];
 $score = $_POST['score'];
