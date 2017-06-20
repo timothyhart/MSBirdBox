7f6566af2f622e7c6e9becfaf8550a05feaf17a3
Tim <timothy.hart@my.jcu.edu.au>
2017-06-20 12:18:30 +1000

    Added register to login.php

--- learning-module/js/login.js
+++ learning-module/js/login.js
@@ -2,19 +2,23 @@ function LoginView(){
   var view = this;
     view.container = $(".login-view");
     view.container.find(".login-button").click(function (e){ view.onLoginButtonPressed(); });
+    view.container.find(".register-button").click(function (e){ view.onRegisterButtonPressed(); });
     view.loginForm = view.container.find(".login");
-    view.IDTextField = view.loginForm.find(".username");
-    view.pwdTextField = view.loginForm.find(".password");
+    view.regForm = view.container.find(".register");
+    view.IDLoginTextField = view.loginForm.find(".username");
+    view.pwdLoginTextField = view.loginForm.find(".password");
+    view.nameRegTextField = view.loginForm.find(".username");
+    view.pwdRegTextField = view.loginForm.find(".password");
 }
 
 LoginView.prototype.onLoginButtonPressed = function () {
 
   var view = this;
-  var userID = view.IDTextField.val();
-  var password = view.pwdTextField.val();
+  var userID = view.IDLoginTextField.val();
+  var password = view.pwdLoginTextField.val();
 
 
-  var data = {"userID": userID, "password": password };
+  var data = {"userID": userID, "password": password, "action": 0 };
 
 
   var loginURL = "js/login.php";
@@ -29,7 +33,7 @@ LoginView.prototype.onLoginButtonPressed = function () {
         sessionStorage.setItem('userID', res[1]);
         sessionStorage.setItem('level', res[2]);
         sessionStorage.setItem('isAdmin', res[3]);
-        console.log(g_views);
+        //console.log(g_views);
         switchView(g_views.titleView);
 	      location.reload();
       }
@@ -39,5 +43,29 @@ alert("Incorrect username or password");
     }
   })
 };
+
+LoginView.prototype.onRegisterButtonPressed = function () {
+  var view = this;
+  var name = view.nameRegTextField.val();
+  var password = view.pwdRegTextField.val();
+
+
+  var data = {"name": name, "password": password, "action": 1 };
+
+
+  var loginURL = "js/login.php";
+  $.ajax({
+    url: loginURL,
+    method: 'POST',
+    data: data,
+    success: function(res){
+    alert(res);
+      }
+else {
+alert("Incorrect username or password");
+}
+    }
+  })
+}
 
 g_views.loginView = new LoginView();
--- learning-module/js/login.php
+++ learning-module/js/login.php
@@ -1,6 +1,8 @@
 <?php
 $userID = intval($_POST["userID"]);
 $pwd = $_POST["password"];
+$action = intval($_POST["action"]);
+$name = $_POST["name"];
 
 
 $con = mysqli_connect('localhost','birdbox','birdbox','birdbox');
@@ -8,16 +10,30 @@ if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
 }
 
-$sql = "SELECT * FROM User WHERE userID = '$userID'";
+switch($action){
+  case 0: $sql = "SELECT * FROM User WHERE userID = '$userID'";
 
-$result = mysqli_query($con, $sql);
-$row = mysqli_fetch_row($result);
-if($row[2] === $pwd){
-  $data = array(1, $userID, $row[4], $row[5]);
-} else {
-    $data = array(0);
+  $result = mysqli_query($con, $sql);
+  $row = mysqli_fetch_row($result);
+  if($row[2] === $pwd){
+    $data = array(1, $userID, $row[4], $row[5]);
+  } else {
+      $data = array(0);
+  }
+  echo json_encode($data);
+  break;
+  case 1: $maxsql = "SELECT MAX(userID) AS userID FROM User";
+  $result = mysqli_query($con, $maxsql);
+  $row = mysqli_fetch_row($result);
+  $maxID = $row[0] + 1;
+
+  $sql = "INSERT INTO User (userID, name, password) VALUES($maxID, $name, $pwd )";
+ if(mysqli_query($con, $sql)){
+   echo "Success your userID is: $userID";
+ };
+
 }
-echo json_encode($data);
+
 
 
 
