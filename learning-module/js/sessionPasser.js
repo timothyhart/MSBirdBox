797d55b731c854d276e8c222a4da2c6db823ab4b
Tim <timothy.hart@my.jcu.edu.au>
2017-06-22 10:47:02 +1000

    Passing session variables around like a pack of ciggies in prison

--- learning-module/js/sessionPasser.js
+++ learning-module/js/sessionPasser.js
@@ -1,4 +1,5 @@
 var PassSession = function(){
+  console.log("Passing the session");
   if(sessionStorage.getItem("userID") === null && sessionStorage.getItem("isAdmin") === null){
     localStorage.setItem("userID", -1);
     localStorage.setItem("isAdmin", 0);
@@ -9,6 +10,7 @@ var PassSession = function(){
 }
 
 var RetrieveSession = function (){
+  console.log("Retrieving the session");
   if(localStorage.getItem("userID") === null && localStorage.getItem("isAdmin") === null){
     sessionStorage.setItem("userID", -1);
     sessionStorage.setItem("isAdmin", 0);
