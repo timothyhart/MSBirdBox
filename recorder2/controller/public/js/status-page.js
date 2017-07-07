4a93f1e83a63fa9e2600d3e44df11963630de462
Tim <timothy.hart@my.jcu.edu.au>
2017-07-07 14:08:41 +1000

    Hiding manual recorder if not admin

--- recorder2/controller/public/js/status-page.js
+++ recorder2/controller/public/js/status-page.js
@@ -14,16 +14,16 @@ function StatusPage(root, navItem, hashTracker) {
     view.refreshInterval = 30000;
     view.refreshTime = new Date();
     view.refreshTimer = null;
-    
+
     // Set a sensible default manual recording name.
     view.manualRecordingPanel.find(".recording-title").val("Manual Recording " + moment().format("YYYY-MM-DD HH:mm:ss"));
-    
+
     // Hide everything initially.
     view.summaryPanel.children().hide();
     view.currentRecordingPanel.hide();
     view.nextRecordingPanel.hide();
     view.manualRecordingPanel.hide();
-    view.recentRecordingsPanel.hide();    
+    view.recentRecordingsPanel.hide();
 
     // Event hooks
     hashTracker.registerAction("status-page", function(action, params) { return view.onHashChanged(action, params); });
@@ -33,7 +33,10 @@ function StatusPage(root, navItem, hashTracker) {
 
 StatusPage.prototype.onActivate = function() {
     this.hashTracker.setAction("status-page");
-    
+
+    if(parseInt(sessionStorage.getItem("isAdmin")) !== 1){
+      view.manualRecordingPanel.hide();
+    }
     // Refresh, or set refresh timer.
     if (this.isLoaded)
         this.setRefreshTimer();
@@ -69,19 +72,19 @@ StatusPage.prototype.onStartManualRecordingButtonPressed = function() {
         toastr["error"]("Cannot record without specifying a duration.", "Record Error");
         return;
     }
-    
+
     beginLoadingModal();
-    
-    $.getJSON(BACKEND_URL + "/recordings/start-manual-recording?title=" + title + 
+
+    $.getJSON(BACKEND_URL + "/recordings/start-manual-recording?title=" + title +
                             "&hours=" + hours + "&minutes=" + minutes + "&seconds=" + seconds,
         function(result) {
             console.log("manual recording result", result);
             if (result.result) {
                 // Recording started.
                 toastr["success"](result.title, "Manual Recording Started");
-                
+
                 // Wait half a second before refreshing the view, in case the recorder process failed to start.
-                setTimeout(function() { view.refresh(); endLoadingModal(); }, 500);                
+                setTimeout(function() { view.refresh(); endLoadingModal(); }, 500);
             } else {
                 // Failed to start.
                 toastr["error"]("Failed to start recording: " + result.error, "Record Error");
@@ -91,7 +94,7 @@ StatusPage.prototype.onStartManualRecordingButtonPressed = function() {
     ).fail(function() {
         toastr["error"]("Failed to send recording start request.", "Record Error");
         endLoadingModal();
-    });        
+    });
 }
 
 StatusPage.prototype.setRefreshTimer = function() {
@@ -100,7 +103,7 @@ StatusPage.prototype.setRefreshTimer = function() {
         clearTimeout(view.refreshTimer);
         view.refreshTimer = null;
     }
-    
+
     // Find delay to refresh in.
     var delay = view.refreshInterval - (new Date() - view.refreshTime);
     if (delay <= 0) {
@@ -115,26 +118,26 @@ StatusPage.prototype.setRefreshTimer = function() {
 
 StatusPage.prototype.refresh = function(wasManualRefresh) {
     var view = this;
-    
+
     // Prevent infinite reloading.
     view.refreshButton.attr("disabled", "disabled");
     view.refreshTime = new Date();
     view.isLoaded = true;
-    
+
     //console.log("refreshing status view");
-    
+
     var errorHandler = function(err) {
         toastr["error"](err, "Refresh Error");
-        
+
         // Hide everything.
         view.summaryPanel.children().hide();
         view.currentRecordingPanel.hide();
         view.nextRecordingPanel.hide();
         view.manualRecordingPanel.hide();
         view.recentRecordingsPanel.hide();
-    
+
         endLoadingModal();
-        
+
         view.refreshButton.removeAttr("disabled");
         view.refreshTime = new Date();
         view.setRefreshTimer();
@@ -145,17 +148,17 @@ StatusPage.prototype.refresh = function(wasManualRefresh) {
         endLoadingModal();
         if (wasManualRefresh)
             toastr["info"]("Status page updated.");
-        
+
         view.refreshButton.removeAttr("disabled");
         view.refreshTime = new Date();
         view.setRefreshTimer();
     };
-    
+
     // Start by fetching the current recording status
     beginLoadingModal();
     $.getJSON(BACKEND_URL + "/status/current-recording", function(result) {
         console.log("current recording status", result);
-        
+
         view.summaryPanel.children().hide();
         var isRecording = (result.state == "recording");
         if (isRecording) {
@@ -164,8 +167,8 @@ StatusPage.prototype.refresh = function(wasManualRefresh) {
             view.currentRecordingPanel.find(".end-time").text(moment(result.endTime).format(DATETIME_DISPLAY_FORMAT));
             view.currentRecordingPanel.find(".elapsed-time").text(moment.duration(result.elapsedTime, "seconds").format(DURATION_DISPLAY_FORMAT));
             view.currentRecordingPanel.find(".remaining-time").text(moment.duration(result.remainingTime, "seconds").format(DURATION_DISPLAY_FORMAT));
-            
-            // Show appropriate panels.            
+
+            // Show appropriate panels.
             view.summaryPanel.find(".status-perfect").show();
             view.currentRecordingPanel.show();
             view.nextRecordingPanel.hide();
@@ -174,15 +177,15 @@ StatusPage.prototype.refresh = function(wasManualRefresh) {
             // Show the manual recording box, since we're not recording.
             view.currentRecordingPanel.hide();
             view.manualRecordingPanel.show();
-        }            
-        
+        }
+
         // Get next recording state.
         $.getJSON(BACKEND_URL + "/status/next-recording", function(result) {
             if (result.time !== undefined) {
                 // Has a next recording time.
                 if (!isRecording)
                     view.summaryPanel.find(".status-reduced").show();
-                    
+
                 view.nextRecordingPanel.find(".next-time").text(moment(result.time).format(DATETIME_DISPLAY_FORMAT));
                 view.nextRecordingPanel.find(".next-duration").text(moment.duration(result.duration, "seconds").format(DURATION_DISPLAY_FORMAT));
                 view.nextRecordingPanel.show();
@@ -190,20 +193,20 @@ StatusPage.prototype.refresh = function(wasManualRefresh) {
                 // No next recording.
                 if (!isRecording)
                     view.summaryPanel.find(".status-critical").show();
-                    
+
                 view.nextRecordingPanel.hide();
             }
-            
+
             // Everything is loaded now.
             completeHandler();
         }).fail(function() {
             errorHandler("Failed to load next recording state");
         });
-        
+
     }).fail(function() {
         errorHandler("Failed to load current recording state");
     });
-    
+
     // Fetch recent recordings
     beginLoadingModal();
     $.getJSON(BACKEND_URL + "/recordings/recent", function(data) {
@@ -223,7 +226,7 @@ StatusPage.prototype.refresh = function(wasManualRefresh) {
 
             var buttonColumn = $("<td>");
             buttonColumn.attr("class", "text-right");
-            
+
             var viewButton = $("<button>").attr("class", "btn btn-sm btn-primary xs-left-margin xs-bottom-margin").html("<i class='glyphicon glyphicon-folder-open sm-right-margin'></i>View");
             viewButton.click(function() { g_recordingViewer.openRecording(value.name); });
             viewButton.appendTo(buttonColumn);
@@ -231,7 +234,7 @@ StatusPage.prototype.refresh = function(wasManualRefresh) {
 
             recentTable.append(row);
         });
-        
+
         // Done loading something else
         view.recentRecordingsPanel.show();
         endLoadingModal();
